dc32d81b1a120f5e52fe60a1779c98c95f3dd071
Snow Aye <saungaye7@gmail.com>
2019-09-14 11:52:45 +0630

    fragment_detail added

---
+++ app/src/main/java/com/padc/batch9/assignment6/activity/RestaurantDetailActivity.java
@@ -0,0 +1,15 @@
+package com.padc.batch9.assignment6.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.padc.batch9.assignment6.R;
+
+public class RestaurantDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_detail);
+    }
+}
